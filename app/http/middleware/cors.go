094a22119a8fe7e5dd1c5a22aa5c3299f1e240cb
Billdex <pc.luoxin@foxmail.com>
2022-01-11 10:30:56 +0800

    修改cors

--- app/http/middleware/cors.go
+++ app/http/middleware/cors.go
@@ -3,10 +3,19 @@ package middleware
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func Cors() gin.HandlerFunc {
-	conf := cors.DefaultConfig()
-	conf.AllowOrigins = []string{}
-	return cors.New(conf)
+	return cors.New(cors.Config{
+		// 允许的请求来源
+		AllowOrigins: []string{"localhost", "http://127.0.0.1"},
+		// 允许的请求方法
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		// 请求头应当携带的字段
+		AllowHeaders: []string{"Origin", "Content-Length", "Content-Type"},
+		// 是否允许携带 Cookie 等身份信息
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
 }
