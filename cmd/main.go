aee319968dbffa09334fa9a14f2f9416b995f0ab
Billdex <pc.luoxin@foxmail.com>
2022-01-12 18:11:01 +0800

    http server 优雅退出

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
 	"ready-go/boot"
 	"ready-go/pkg/logger"
+	"ready-go/router"
 	"runtime"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -14,4 +21,29 @@ func main() {
 		logger.Fatal(err)
 	}
 
+	// 初始化 http 服务
+	r := router.InitRouter()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 优雅退出
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	logger.Info("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Fatal("Server Shutdown:", err)
+	}
+	logger.Info("Server exiting")
 }
