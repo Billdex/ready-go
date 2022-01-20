29fe2b1ea15c184b455e5ff38167124c23e0befe
Billdex <pc.luoxin@foxmail.com>
2022-01-20 11:48:43 +0800

    增加分页中间件

---
+++ app/http/middleware/paginate.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"ready-go/app/http/response"
+	"ready-go/pkg/util/numutil"
+	"strconv"
+)
+
+func Paginate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+		if err != nil {
+			response.ErrorValidateParamsFail(c)
+			c.Abort()
+			return
+		}
+		page = numutil.IntLeast(page, 1)
+		pageSize, err := strconv.Atoi(c.DefaultQuery("page_size", "25"))
+		if err != nil {
+			response.ErrorValidateParamsFail(c)
+			c.Abort()
+			return
+		}
+		pageSize = numutil.IntBetween(pageSize, 1, 100)
+		c.Set("page", page)
+		c.Set("page_size", pageSize)
+		c.Next()
+	}
+}
